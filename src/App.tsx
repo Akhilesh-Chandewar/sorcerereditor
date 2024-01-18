61291ee2f27c80bd27df5c221051c84a3c80e47c
Akhilesh Chandewar <AKHIL.CHANDEWAR00@GMAIL.COM>
2024-01-18 14:06:20 +0530

    Prototype

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,131 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef } from "react";
+import "./App.css";
+import {
+  ContentBlock,
+  Editor,
+  EditorState,
+  convertFromRaw,
+  convertToRaw,
+} from "draft-js";
+import { handleHeading } from "./logics/headerLogic";
+import { handleUnderline } from "./logics/underlineLogic";
+import { handleRed } from "./logics/redLogic";
+import { handleBold } from "./logics/boldLogic";
 
 function App() {
+  const [editorState, setEditorState] = React.useState(() =>
+    EditorState.createEmpty()
+  );
+  const childRef = useRef(null);
+  useEffect(() => {
+    let localItem = localStorage.getItem("editorContext");
+    if (localItem) {
+      const context = convertFromRaw(JSON.parse(localItem));
+      console.log(context.toObject());
+      setEditorState(EditorState.createWithContent(context));
+    }
+  }, []);
+  const handleChange = (editorState: EditorState) => {
+    const selection = editorState.getSelection();
+    const content = editorState.getCurrentContent();
+    const block = content.getBlockForKey(selection.getStartKey());
+    const text = block.getText();
+    const contentState = editorState.getCurrentContent();
+    const selectionState = editorState.getSelection();
+    const blockKey = block.getKey();
+    if (text.startsWith("# ")) {
+      const newEditorState = handleHeading(
+        selectionState,
+        contentState,
+        block,
+        text,
+        blockKey,
+        editorState
+      );
+      setEditorState(newEditorState);
+      return true;
+    } else if (text.startsWith("*** ")) {
+      const newEditorState = handleUnderline(
+        selectionState,
+        contentState,
+        block,
+        text,
+        blockKey,
+        editorState
+      );
+      setEditorState(newEditorState);
+      return true;
+    } else if (text.startsWith("** ")) {
+      const newEditorState = handleRed(
+        selectionState,
+        contentState,
+        block,
+        text,
+        blockKey,
+        editorState
+      );
+      setEditorState(newEditorState);
+      return true;
+    } else if (text.startsWith("* ")) {
+      const newEditorState = handleBold(
+        selectionState,
+        contentState,
+        block,
+        text,
+        blockKey,
+        editorState
+      );
+      setEditorState(newEditorState);
+      return true;
+    }
+    setEditorState(editorState);
+
+    return false;
+  };
+  const handleSave = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    e.stopPropagation();
+    localStorage.setItem(
+      "editorContext",
+      JSON.stringify(convertToRaw(editorState.getCurrentContent()))
+    );
+    // console.log(editorState.getCurrentContent().toObject());
+  };
+  function myBlockStyleFn(contentBlock: ContentBlock) {
+    const type = contentBlock.getType();
+    if (type === "block-bold") {
+      return "BoldBlock";
+    }
+    if (type === "block-red") {
+      return "RedBlock";
+    }
+    if (type === "block-underline") {
+      return "UnderlineBlock";
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="titleBar">
+        <span></span>
+        <p>SorcererEditor by Akhilesh Chandewar</p>
+        <button onClick={handleSave}>Save</button>
+      </div>
+
+      <div
+        onClick={() => {
+          //@ts-ignore
+          childRef.current && childRef.current.focus();
+        }}
+        className="editor"
+      >
+        <Editor
+          ref={childRef}
+          editorState={editorState}
+          onChange={handleChange}
+          //@ts-ignore
+          blockStyleFn={myBlockStyleFn}
+        />
+      </div>
     </div>
   );
 }
